f61c20e06eac046bacc5eb3f0372a333c18ef8d7
Eugene Plasmon <bewave@yandex.ru>
2021-12-22 22:59:27 +0300

    Debug subsystem added

---
+++ ue4/Eix/Source/EixCore/Private/Subsystems/DebugSubsystem.cpp
@@ -0,0 +1,38 @@
+﻿#include "Subsystems/DebugSubsystem.h"
+
+namespace DebugCategory
+{
+	extern const FName IKCharacter("IKCharacter");
+
+	extern const FName All("All");
+
+	TArray<FName> AllCategories =
+	{
+		IKCharacter
+	};
+}
+
+bool UDebugSubsystem::IsDebugCategoryEnabled(const FName& Category) const
+{
+#if UE_BUILD_SHIPPING || UE_BUILD_TEST
+	return false;
+#else
+	const bool* bEnabledPtr = DebugCategoryEnabledInfo.Find(Category);
+	return bEnabledPtr != nullptr && *bEnabledPtr;
+#endif
+}
+
+void UDebugSubsystem::DebugCategorySetEnabled(const FName& Category_In, bool bEnabled_In)
+{
+	if (Category_In == DebugCategory::All)
+	{
+		for (FName CategoryName : DebugCategory::AllCategories)
+		{
+			DebugCategorySetEnabled(CategoryName, bEnabled_In);
+		}
+		return;
+	}
+	
+	bool& bEnabled = DebugCategoryEnabledInfo.FindOrAdd(Category_In);
+	bEnabled = bEnabled_In;
+}
---
+++ ue4/Eix/Source/EixCore/Public/Subsystems/DebugSubsystem.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "DebugSubsystem.generated.h"
+
+namespace DebugCategory
+{
+	extern const FName IKCharacter;
+
+	extern const FName All;
+}
+
+UCLASS()
+class EIXCORE_API UDebugSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	bool IsDebugCategoryEnabled(const FName& Category) const;
+	
+private:
+	UFUNCTION(Exec)
+	void DebugCategorySetEnabled(const FName& Category_In, bool bEnabled_In);
+
+	TMap<FName, bool> DebugCategoryEnabledInfo;
+};

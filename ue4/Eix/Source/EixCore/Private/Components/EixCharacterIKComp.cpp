53407ec94d466260add3621f9322bc8abf5cbd66
Eugene Plasmon <bewave@yandex.ru>
2021-12-22 22:59:44 +0300

    IK component to use in character added

---
+++ ue4/Eix/Source/EixCore/Private/Components/EixCharacterIKComp.cpp
@@ -0,0 +1,102 @@
+﻿#include "Components/EixCharacterIKComp.h"
+#include "Subsystems/DebugSubsystem.h"
+
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+void UEixCharacterIKComp::BeginPlay()
+{
+	Super::BeginPlay();
+	checkf(GetOwner()->IsA<ACharacter>(), TEXT("UEixCharacterIKComponent works only with ACharacter"));
+	CharacterOwner = StaticCast<ACharacter*>(GetOwner());
+}
+
+bool UEixCharacterIKComp::NeedCalculateIK() const
+{
+	if (!CharacterOwner.IsValid())
+	{
+		return false;
+	}
+	return CharacterOwner->GetCharacterMovement()->IsMovingOnGround();
+}
+
+void UEixCharacterIKComp::UpdateIK(float DeltaTime)
+{
+	const bool bResetIK = !NeedCalculateIK()
+		|| LeftFootSocketName.IsNone()
+		|| RightFootSocketName.IsNone();
+
+	if (bResetIK)
+	{
+		TargetIKOffset_Pelvis = FVector(0.f);
+		TargetIKOffset_LeftFoot = FVector(0.f);
+		TargetIKOffset_RightFoot = FVector(0.f);
+		TargetIKRotation_LeftFoot = FRotator(0.f);
+		TargetIKRotation_RightFoot = FRotator(0.f);
+	}
+	else
+	{
+		FVector FloorOffset_LeftFoot, FloorOffset_RightFoot;
+		GetIKFootOffsetAndRotationForSocket(LeftFootSocketName, FloorOffset_LeftFoot, TargetIKRotation_LeftFoot);
+		GetIKFootOffsetAndRotationForSocket(RightFootSocketName, FloorOffset_RightFoot, TargetIKRotation_RightFoot);
+		if (FloorOffset_LeftFoot.Z < FloorOffset_RightFoot.Z)
+		{
+			TargetIKOffset_Pelvis = FloorOffset_LeftFoot;
+		}
+		else
+		{
+			TargetIKOffset_Pelvis = FloorOffset_RightFoot;
+		}
+		TargetIKOffset_LeftFoot = FloorOffset_LeftFoot - TargetIKOffset_Pelvis;
+		TargetIKOffset_RightFoot = FloorOffset_RightFoot - TargetIKOffset_Pelvis;
+
+		if (bDisableIKFootRotationWhileMoving)
+		{
+			const float CharacterAccelerationSqr = CharacterOwner->GetCharacterMovement()->GetCurrentAcceleration().SizeSquared();
+			if (CharacterAccelerationSqr > SMALL_NUMBER)
+			{
+				TargetIKRotation_LeftFoot = FRotator(0.f);
+				TargetIKRotation_RightFoot = FRotator(0.f);
+			}
+		}
+	}
+
+	IKOffset_Pelvis = FMath::VInterpTo(IKOffset_Pelvis, TargetIKOffset_Pelvis, DeltaTime, IKOffsetInterpSpeed);
+	IKOffset_LeftFoot = FMath::VInterpTo(IKOffset_LeftFoot, TargetIKOffset_LeftFoot, DeltaTime, IKOffsetInterpSpeed);
+	IKOffset_RightFoot = FMath::VInterpTo(IKOffset_RightFoot, TargetIKOffset_RightFoot, DeltaTime, IKOffsetInterpSpeed);
+	IKRotation_LeftFoot = FMath::RInterpTo(IKRotation_LeftFoot, TargetIKRotation_LeftFoot, DeltaTime, IKRotationInterpSpeed);
+	IKRotation_RightFoot = FMath::RInterpTo(IKRotation_RightFoot, TargetIKRotation_RightFoot, DeltaTime, IKRotationInterpSpeed);
+}
+
+void UEixCharacterIKComp::GetIKFootOffsetAndRotationForSocket(const FName& SocketName, FVector& OutOffset, FRotator& OutRotation) const
+{
+	FHitResult Hit;
+	const FVector RootLocation = CharacterOwner->GetMesh()->GetSocketLocation(RootBoneName);
+	const FVector SocketLocation = CharacterOwner->GetMesh()->GetSocketLocation(SocketName);
+	const FVector TraceFloorLocation = FVector(SocketLocation.X, SocketLocation.Y, RootLocation.Z);
+	const FVector TraceStart = TraceFloorLocation + FVector(0.f, 0.f, TraceHalfLength);
+	const FVector TraceEnd = TraceFloorLocation - FVector(0.f, 0.f, TraceHalfLength);
+	const ETraceTypeQuery TraceTypeQuery = UEngineTypes::ConvertToTraceType(ECollisionChannel::ECC_Visibility);
+	const bool bDrawDebug = GetWorld()->GetGameInstance()->GetSubsystem<UDebugSubsystem>()
+	                                  ->IsDebugCategoryEnabled(DebugCategory::IKCharacter);
+	const EDrawDebugTrace::Type DrawDebugTrace = bDrawDebug ? EDrawDebugTrace::ForOneFrame : EDrawDebugTrace::None;
+	const bool bGotHit = UKismetSystemLibrary::LineTraceSingle(GetWorld(), TraceStart, TraceEnd,
+	                                                           TraceTypeQuery, true, TArray<AActor*>(),
+	                                                           DrawDebugTrace, Hit, true);
+	if (bGotHit && CharacterOwner->GetCharacterMovement()->IsWalkable(Hit))
+	{
+		const FVector FootInitialLocation = TraceFloorLocation + FootHeight * FVector::UpVector;
+		const FVector FootTargetLocation = Hit.ImpactPoint + Hit.ImpactNormal * FootHeight;
+		OutOffset = FootTargetLocation - FootInitialLocation;
+
+		const float PitchRadians = -FMath::Atan2(Hit.ImpactNormal.X, Hit.ImpactNormal.Z);
+		const float RollRadians = FMath::Atan2(Hit.ImpactNormal.Y, Hit.ImpactNormal.Z);
+		OutRotation = FRotator(FMath::RadiansToDegrees(PitchRadians), 0.f, FMath::RadiansToDegrees(RollRadians));
+	}
+	else
+	{
+		OutOffset = FVector(0.f);
+		OutRotation = FRotator(0.f);
+	}
+}
---
+++ ue4/Eix/Source/EixCore/Public/Components/EixCharacterIKComp.h
@@ -0,0 +1,68 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "EixCharacterIKComp.generated.h"
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class EIXCORE_API UEixCharacterIKComp : public UActorComponent
+{
+	GENERATED_BODY()
+	
+public:
+	void UpdateIK(float DeltaTime);
+
+	FVector FORCEINLINE GetIKOffset_Pelvis() const { return IKOffset_Pelvis; }
+	FVector FORCEINLINE GetIKOffset_LeftFoot() const { return IKOffset_LeftFoot; }
+	FVector FORCEINLINE GetIKOffset_RightFoot() const { return IKOffset_RightFoot; }
+	FRotator FORCEINLINE GetIKRotation_LeftFoot() const { return IKRotation_LeftFoot; }
+	FRotator FORCEINLINE GetIKRotation_RightFoot() const { return IKRotation_RightFoot; }
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly, Category = "IK|Names")
+	FName RootBoneName = FName("root");
+
+	UPROPERTY(EditDefaultsOnly, Category = "IK|Names")
+	FName LeftFootSocketName = FName("ik_foot_l_socket");
+
+	UPROPERTY(EditDefaultsOnly, Category = "IK|Names")
+	FName RightFootSocketName = FName("ik_foot_r_socket");
+
+	UPROPERTY(EditDefaultsOnly, Category = "IK")
+	bool bDisableIKFootRotationWhileMoving = true;
+
+	UPROPERTY(EditDefaultsOnly, Category = "IK")
+	float TraceHalfLength = 50.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "IK")
+	float IKOffsetInterpSpeed = 15.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "IK")
+	float IKRotationInterpSpeed = 40.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "IK")
+	float FootHeight = 13.5f;
+
+private:
+	TWeakObjectPtr<class ACharacter> CharacterOwner;
+
+	// Interpolated offsets/rotations
+	FVector IKOffset_Pelvis;
+	FVector IKOffset_LeftFoot;
+	FVector IKOffset_RightFoot;
+	FRotator IKRotation_LeftFoot;
+	FRotator IKRotation_RightFoot;
+
+	// Target offsets/rotations
+	FVector TargetIKOffset_Pelvis;
+	FVector TargetIKOffset_LeftFoot;
+	FVector TargetIKOffset_RightFoot;
+	FRotator TargetIKRotation_LeftFoot;
+	FRotator TargetIKRotation_RightFoot;
+
+	bool NeedCalculateIK() const;
+
+	void GetIKFootOffsetAndRotationForSocket(const FName& SocketName, OUT FVector& OutOffset, OUT FRotator& OutRotation) const;
+};

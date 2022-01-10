5e65a2238d4021cc6cbca7a12238f284593421dc
Eugene Plasmon <bewave@yandex.ru>
2022-01-10 14:40:36 +0300

    Custom CameraManager blank class (behavior be implemented later)

---
+++ ue4/Eix/Source/EixCore/Private/Characters/EixPlayerCameraManager.cpp
@@ -0,0 +1,35 @@
+﻿#include "Characters/EixPlayerCameraManager.h"
+
+#include "Characters/EixPlayerCharacter.h"
+
+AEixPlayerCameraManager::AEixPlayerCameraManager()
+{
+	ViewPitchMin = -80.f;
+	ViewPitchMax = 30.f;
+}
+
+void AEixPlayerCameraManager::UpdateViewTargetInternal(FTViewTarget& OutVT, float DeltaTime)
+{
+	if (!IsValid(OutVT.Target))
+	{
+		return;
+	}
+	checkf(OutVT.Target->IsA<AEixPlayerCharacter>(), TEXT("AEixPlayerCameraManager can only work with AEixPlayerCharacter"));
+
+	//TODO: Custom camera implementation. Use commented code below as a template
+	Super::UpdateViewTargetInternal(OutVT, DeltaTime);
+
+	//FVector OutLocation;
+	//FRotator OutRotation;
+	//float OutFOV;
+	//if (BlueprintUpdateCamera(OutVT.Target, OutLocation, OutRotation, OutFOV))
+	//{
+	//	OutVT.POV.Location = OutLocation;
+	//	OutVT.POV.Rotation = OutRotation;
+	//	OutVT.POV.FOV = OutFOV;
+	//}
+	//else
+	//{
+	//	OutVT.Target->CalcCamera(DeltaTime, OutVT.POV);
+	//}
+}
---
+++ ue4/Eix/Source/EixCore/Public/Characters/EixPlayerCameraManager.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/PlayerCameraManager.h"
+#include "EixPlayerCameraManager.Generated.h"
+
+UCLASS()
+class AEixPlayerCameraManager : public APlayerCameraManager
+{
+	GENERATED_BODY()
+	
+public:
+	AEixPlayerCameraManager();
+
+protected:
+	virtual void UpdateViewTargetInternal(FTViewTarget& OutVT, float DeltaTime) override;
+};

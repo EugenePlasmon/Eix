2c785861ca2be3e35716b347889a8e9c6c8a4303
Eugene Plasmon <bewave@yandex.ru>
2022-01-27 01:13:52 +0300

    Utils for sweep (and possibly other) tests

---
+++ ue4/Eix/Source/EixCore/Private/Utils/EixShapeUtils.cpp
@@ -0,0 +1,13 @@
+﻿#include "Utils/EixShapeUtils.h"
+
+namespace EixShapeUtils
+{
+	CapsuleParams MakeCapsuleSweepingSphere(FVector Start, FVector End, float Radius)
+	{
+		const FVector Center = (Start + End) / 2.f;
+		const FVector CapsuleUpVector = End - Start;
+		const float HalfHeight = CapsuleUpVector.Size() / 2.f + Radius;
+		const FQuat Quat = FRotationMatrix::MakeFromZ(CapsuleUpVector).ToQuat();
+		return CapsuleParams(Center, Quat, Radius, HalfHeight);
+	}
+}
---
+++ ue4/Eix/Source/EixCore/Public/Utils/EixTraceUtils.h
@@ -0,0 +1,55 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+
+namespace EixTraceUtils
+{
+	struct DebugDrawParams
+	{
+		bool bDrawDebug;
+		FColor TraceColor;
+		FColor HitColor;
+		float DrawTime;
+
+		static DebugDrawParams NoDebugDraw;
+
+		DebugDrawParams(bool bDrawDebug, FColor TraceColor, FColor HitColor, float DrawTime = 0.f)
+			: bDrawDebug(bDrawDebug), TraceColor(TraceColor), HitColor(HitColor), DrawTime(DrawTime)
+		{}
+		
+		DebugDrawParams(FColor TraceColor, FColor HitColor, float DrawTime = 0.f)
+			: bDrawDebug(true), TraceColor(TraceColor), HitColor(HitColor), DrawTime(DrawTime)
+		{}
+
+	private:
+		DebugDrawParams()
+			: bDrawDebug(false), DrawTime(0.f)
+		{}
+	};
+	
+	bool SweepSphereSingleByChannel
+	(
+		const UWorld* World,
+		OUT struct FHitResult& OutHit,
+		const FVector& Start,
+		const FVector& End,
+		float Radius,
+		ECollisionChannel TraceChannel,
+		const DebugDrawParams& DebugDrawParams = DebugDrawParams::NoDebugDraw,
+		const FCollisionQueryParams& QueryParams = FCollisionQueryParams::DefaultQueryParam,
+		const FCollisionResponseParams& ResponseParam = FCollisionResponseParams::DefaultResponseParam
+	);
+
+	bool SweepSphereMultiByChannel
+	(
+		const UWorld* World,
+		OUT TArray<struct FHitResult>& OutHits,
+		const FVector& Start,
+		const FVector& End,
+		float Radius,
+		ECollisionChannel TraceChannel,
+		const DebugDrawParams& DebugDrawParams = DebugDrawParams::NoDebugDraw,
+		const FCollisionQueryParams& QueryParams = FCollisionQueryParams::DefaultQueryParam,
+		const FCollisionResponseParams& ResponseParam = FCollisionResponseParams::DefaultResponseParam
+	);
+}
